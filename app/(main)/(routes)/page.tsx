cb4b37c96f03d549d37941c395604803efc9b5c4
Eugene Polyakov <jenick12345@yandex.ru>
2023-10-23 13:08:02 +0300

    authentication is added via clerk library

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
     return (
         <div className="flex flex-col">
-            <p className="text-3xl font-bold text-indigo-500">Hello world</p>
-            <Button>Click me</Button>
+            <UserButton afterSignOutUrl="/" />
         </div>
     );
 }
