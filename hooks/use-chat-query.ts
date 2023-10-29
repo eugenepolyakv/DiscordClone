77217925ca37ab0c47a0a09bf44e25fdcce2fad3
Eugene Polyakov <jenick12345@yandex.ru>
2023-10-29 19:42:10 +0300

    chat messages component

---
+++ hooks/use-chat-query.ts
@@ -0,0 +1,37 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { useSocket } from '@/components/providers/socket-provider';
+
+interface ChatQueryProps {
+    queryKey: string;
+    apiUrl: string;
+    paramKey: 'channelId' | 'conversationId';
+    paramValue: string;
+}
+
+export const useChatQuery = ({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+}: ChatQueryProps) => {
+    const { isConnected } = useSocket();
+
+    const fetchMessages = async ({ pageParam = undefined }) => {
+        const res = await fetch(
+            `${apiUrl}?cursor=${pageParam}&${paramKey}=${paramValue}`
+        );
+
+        return res.json();
+    };
+
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+        useInfiniteQuery({
+            queryKey: [queryKey],
+            queryFn: fetchMessages,
+            getNextPageParam: (lastPage) => lastPage?.nextCursor,
+            refetchInterval: isConnected ? false : 1000,
+        });
+
+    return { data, fetchNextPage, hasNextPage, isFetchingNextPage, status };
+};
